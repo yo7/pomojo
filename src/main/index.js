7c5a1cf82af6696b54f6933870d7477ae459a37b
yo7 <bloom6445@gmail.com>
2017-09-11 02:41:46 +0900

    Remove main-window

--- src/main/index.js
+++ src/main/index.js
@@ -1,10 +1,19 @@
-import { app } from 'electron'
-import MainWindow from './main-window'
+import { app, BrowserWindow } from 'electron'
+
+let mainWindow
 
 app.on('ready', () => {
-  MainWindow.create()
+  const options = {width: 500, height: 600}
+  const document = `file://${__dirname}/../../index.html`
+
+  mainWindow = new BrowserWindow(options)
+  mainWindow.loadURL(document)
 })
 
 app.on('window-all-closed', () => {
-  app.quit()
+  const isDarwin = () => process.platform === 'darwin'
+
+  if (!isDarwin) {
+    app.quit()
+  }
 })
