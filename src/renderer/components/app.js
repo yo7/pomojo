ec12134008bfa598b6fc36ca36ef839a76f6e1f3
yo7 <bloom6445@gmail.com>
2017-09-20 07:42:56 +0900

    Remove react

--- src/renderer/components/app.js
+++
@@ -1,9 +0,0 @@
-import {Component} from 'react'
-
-export default class App extends Component {
-  render() {
-    return (
-      <div>Init</div>
-    )
-  }
-}
--- src/renderer/index.js
+++
@@ -1,7 +0,0 @@
-import {render} from 'react-dom'
-import App from './components/app'
-
-render(
-  <App />,
-  document.getElementById('app')
-)
